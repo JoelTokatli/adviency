9ec1f35af6b9b142cb750b1c21d1888d8af031c6
JoelTokatli <jamescarter6667@gmail.com>
2022-12-06 07:27:50 -0400

    Adviency día 6

---
+++ dia-6-adviency/src/App.jsx
@@ -0,0 +1,11 @@
+import GiftsCard from "./Components/GiftsCard";
+import "./Styles/App.css";
+function App() {
+  return (
+    <div className="App">
+      <GiftsCard />
+    </div>
+  );
+}
+
+export default App;
